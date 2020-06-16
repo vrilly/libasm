5d3e8a1af6c3bc57f048a254a26d7005876c31af
Tom Jans <tjans@fluffderg.me>
2020-06-16 12:53:02 +0200

    ft_read.s ft_write.s: Finished
    They handle errors and work perfectly!

--- ft_write.s
+++ ft_write.s
@@ -1,5 +1,14 @@
 global _ft_write
+extern ___error
 _ft_write:
+	push R15
 	mov rax, 0x2000004
 	syscall
+	jnc .return
+	mov R15, RAX
+	call ___error
+	mov [RAX], R15
+	mov RAX, -1
+.return:
+	pop R15
 	ret
