9c4f571f020058281539ef91341932280aa3b77c
Tom Jans <tjans@fluffderg.me>
2020-10-07 15:51:54 +0200

    Fix strcpy

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -1,8 +1,13 @@
 global _ft_strcpy
 _ft_strcpy:
 	mov RAX, RDI		; Store original dst ptr in RAX to return
+	cmp byte [RSI], 0	; Compare byte in [RSI] data to 0
+	je .end			; End if str empty
 .strloop:
-	movsb				; Copy one string byte and increase ptrs in RDI and RSI
+	movsb			; Copy one string byte and increase ptrs in RDI and RSI
 	cmp byte [RSI], 0	; Compare byte in [RSI] data to 0
 	jne .strloop		; Jump to strloop if not equal
-	ret					; return
+.end:
+	movsb			; Copy one string byte and increase ptrs in RDI and RSI
+	mov RDI, RAX		; Copy NUL
+	ret			; return
